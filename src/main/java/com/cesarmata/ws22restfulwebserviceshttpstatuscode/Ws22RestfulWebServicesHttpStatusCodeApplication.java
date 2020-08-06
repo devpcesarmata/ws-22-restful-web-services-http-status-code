8a36d1fb0d048ada639fb001800e3e4faede8860
César <dev.pcesarmata@gmail.com>
2020-08-05 20:30:41 -0500

    init commit

---
+++ src/main/java/com/cesarmata/ws22restfulwebserviceshttpstatuscode/Ws22RestfulWebServicesHttpStatusCodeApplication.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws22restfulwebserviceshttpstatuscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws22RestfulWebServicesHttpStatusCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws22RestfulWebServicesHttpStatusCodeApplication.class, args);
+	}
+
+}
